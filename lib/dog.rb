f9d01392e82f46569b005688cb89118023e09e4e
Josh Ellis <joshe523@gmail.com>
2020-03-04 05:55:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,22 @@ class Dog
   @@all = []
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
 
   def self.all
     @@all
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.print_all
+    @@all.each { |dog| puts dog }
+  end
+
+  def save
+    @@all << self
   end
 end
